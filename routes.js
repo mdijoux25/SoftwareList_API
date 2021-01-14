0da03a372fb22da0af62258f694686de8e9319f4
mdijoux <45066330+mdijoux25@users.noreply.github.com>
2021-01-14 14:27:15 -0500

    add route + change name

--- routes.js
+++ routes.js
@@ -11,11 +11,18 @@ const apps = require('./SoftwareParserService')
 
 // APIs
 router.get('/', rootPath)
+router.post('/list', listSoft)
 
 // API functions
 function rootPath(req, res, next) {
     res.status(200).send("Successfull connection.")
   }
 
+ function listSoft(req, res, next) {
+    apps
+      .parseListing(req.body)
+      .then(parsed => (parsed ? res.json(parsed) : res.sendStatus(404)))
+      .catch(err => next(err))
+  }
 
   module.exports = router
