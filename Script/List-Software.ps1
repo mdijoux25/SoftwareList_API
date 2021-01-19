65d989d0c750ca9b6dbc0e39ddc063cecb9b0c10
mdijoux <45066330+mdijoux25@users.noreply.github.com>
2021-01-19 15:30:12 -0500

    Test with new json

--- Script/List-Software.ps1
+++ Script/List-Software.ps1
@@ -7,25 +7,18 @@ Function Get-Software() {
     
 
 
-$osver = (Get-WmiObject -Class Win32_OperatingSystem).caption +" "+[Environment]::OSVersion.Version
-$Software=@()
-$Software=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName }| Select-Object DisplayName, DisplayVersion, Publisher | Format-List | Out-String
-
-
-
-
-$apinfo = New-Object psobject -Property @{
-
-    "Hostname"          = $env:COMPUTERNAME
-    "OS"                = $osver
-    "Username"          = $env:USERNAME
-    "Domain"            = $env:USERDOMAIN
-    "Software"          = $Software
-
-}
+$Software=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName}| Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object DisplayName  |Export-Csv -Path "C:\soft.csv" -Delimiter "," -NoTypeInformation -Encoding UTF8
+$osver = (Get-WmiObject -Class Win32_OperatingSystem).caption +" "+[Environment]::OSVersion.Version  
+$apinfo = "" | Select Hostname,OS,Username,Domain
+$apinfo.Hostname = $env:COMPUTERNAME
+$apinfo.OS = $osver
+$apinfo.Username = $env:USERNAME
+$apinfo.Domain = $env:USERDOMAIN
+$json= Import-Csv -Path $env:TEMP+"\soft.csv" | ConvertTo-Json 
+$json += $apinfo| ConvertTo-Json  
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $WebAPI -Method POST -Body ($apinfo | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing
+Invoke-WebRequest -Uri $WebAPI -Method POST -Body $json -ContentType "application/json" -UseBasicParsing
 }
 Set-Alias getsoftware -Value Get-Software | Out-Null
 Export-ModuleMember -Alias 'getsoftware' -Function 'Get-Software' | Out-Null
