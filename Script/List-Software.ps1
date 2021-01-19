235f11a6b63fe61e7839d3e490a0ec4903f187b4
mdijoux <45066330+mdijoux25@users.noreply.github.com>
2021-01-19 15:54:45 -0500

    Test with new json

--- Script/List-Software.ps1
+++ Script/List-Software.ps1
@@ -9,12 +9,12 @@ Function Get-Software() {
 
 $Software=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName}| Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object DisplayName  |Export-Csv -Path "$env:TEMP\soft.csv" -Delimiter "," -NoTypeInformation -Encoding UTF8
 $osver = (Get-WmiObject -Class Win32_OperatingSystem).caption +" "+[Environment]::OSVersion.Version  
-$apinfo = "" | Select Hostname,OS,Username,Domain
+$apinfo = "" | Select Hostname,OS,Username,Domain,Software
 $apinfo.Hostname = $env:COMPUTERNAME
 $apinfo.OS = $osver
 $apinfo.Username = $env:USERNAME
 $apinfo.Domain = $env:USERDOMAIN
-$json= Import-Csv -Path "$env:TEMP\soft.csv"  
+$apinfo.Software = Import-Csv -Path "$env:TEMP\soft.csv"  | ConvertTo-Json
 $json += $apinfo
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
